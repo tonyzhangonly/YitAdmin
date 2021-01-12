982330812fff37c19dbdb175c70a1209f0fe8ec6
Administrator <1329657844@qq.com>
2021-01-12 15:55:10 +0800

    添加dal

---
+++ Yit.Data.Repository/RepositoryBase.cs
@@ -0,0 +1,38 @@
+﻿using SqlSugar;
+using System;
+using Yit.Util;
+
+namespace YiSha.Data.Repository
+{
+    /// <summary>
+    /// SqlSugar仓储
+    /// </summary>
+    public class RepositoryBase<T> : SimpleClient<T> where T : class, new()
+    {
+        /// <summary>
+        /// 创建仓储
+        /// </summary>
+        /// <param name="context"></param>
+        public RepositoryBase(ISqlSugarClient context = null) : base(context)//注意这里要有默认值等于null
+        {
+            string dbType = GlobalContext.SystemConfig.DBProvider;
+            string dbConnectionString = GlobalContext.SystemConfig.DBConnectionString;
+            object sugarDbType = null;
+            var isSugarDb = Enum.TryParse(typeof(DbType), dbType, out sugarDbType);
+            if (!isSugarDb)
+            {
+                throw new Exception("不支持数据库连接类型，连接类型为 MySql,SqlServer,Sqlite,Oracle,PostgreSQL,Dm,Kdbndp");
+            }
+            if (context == null)
+            {
+                base.Context = new SqlSugarClient(new ConnectionConfig()
+                {
+                    DbType = (DbType)sugarDbType,
+                    InitKeyType = InitKeyType.Attribute,
+                    IsAutoCloseConnection = true,
+                    ConnectionString = dbConnectionString
+                });
+            }
+        }
+    }
+}
