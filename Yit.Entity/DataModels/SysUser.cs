5ab39f3afeb6a3d09ef862cb5361b853467bc710
Administrator <1329657844@qq.com>
2021-01-21 15:14:16 +0800

    修改

---
+++ Yit.Entity/DataModels/SysUser.cs
@@ -0,0 +1,26 @@
+﻿/****************************************************************
+* 名称：SysUser
+* 创建人：张思友
+* 创建时间：2021/1/21 14:55:57
+* 修改人：张思友
+* 修改时间：2021/1/21 14:55:57
+* CLR版本：V1.0.0.0
+* 描述说明：
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yit.Entity.DataModels
+{
+    public partial class SysUser
+    {
+        public Guid SysUserId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public int? UserType { get; set; }
+        public int? UserStatus { get; set; }
+        public string Telphone { get; set; }
+    }
+}
---
+++ Yit.Entity/DtoModels/SysUserDto.cs
@@ -0,0 +1,26 @@
+﻿/****************************************************************
+* 名称：SysUserDto
+* 创建人：张思友
+* 创建时间：2021/1/21 14:55:50
+* 修改人：张思友
+* 修改时间：2021/1/21 14:55:50
+* CLR版本：V1.0.0.0
+* 描述说明：
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yit.Entity.DtoModels
+{
+    public partial class SysUserDto
+    {
+        public Guid SysUserId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public int? UserType { get; set; }
+        public int? UserStatus { get; set; }
+        public string Telphone { get; set; }
+    }
+}
